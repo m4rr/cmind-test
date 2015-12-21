706435a4f8143528e0697d2ccf172bfbc3216e40
Marat S <remarr@gmail.com>
2015-12-21 18:53:01 +0300

    Page1ViewController

--- cmind-test/ViewController.swift
+++ cmind-test/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Page1ViewController: UIViewController {
+
+  @IBOutlet weak var webView: UIWebView!
+  @IBOutlet weak var imageView: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+
+    if webView.request == nil {
+      loadGoogle(intoWebView: webView)
+    }
   }
 
+  private func loadGoogle(intoWebView webView: UIWebView) {
+    loadUrl("https://google.com", intoWebView: webView)
+  }
+
+  private func loadYahoo(intoWebView webView: UIWebView) {
+    loadUrl("https://yahoo.com", intoWebView: webView)
+  }
+
+  private func loadUrl(urlString: String, intoWebView webView: UIWebView) {
+    if let url = NSURL(string: urlString) {
+      webView.loadRequest(NSURLRequest(URL: url))
+    }
+  }
 
 }
 
+extension Page1ViewController: UIWebViewDelegate {
+
+  func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    switch navigationType {
+    case .LinkClicked:
+      if request.URL?.host == "yahoo.com"
+      return false
+    }
+  }
+
+//  func webViewDidFinishLoad(webView: UIWebView) {
+//    <#code#>
+//  }
+
+}
